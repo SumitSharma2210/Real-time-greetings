88bf87b630f8f045d2dafc3fa570c9de2584a065
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2024-01-10 20:52:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div>
+      <h1>So this website displays the respective greetings based on your real-time date and time.</h1>
+      <h1>The main purpose of this website is to learn the React components.</h1>
+      <h1>Another important reason is to get used to uploading my code on github.</h1>
+      <h1>CSS has purposefully not been added because the main learning objective was something else and has been fulfilled.</h1>
+      <Heading />
+    </div>
+  );
+}
+export default App;
